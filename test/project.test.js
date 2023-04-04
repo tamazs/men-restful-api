6220a6b8fc32fbcf0ba3d8ca534ee803ce2912d5
Tamás Major <majortamas313@gmail.com>
2023-04-04 16:59:28 +0200

    testing welcome API route

--- test/project.test.js
+++ test/project.test.js
@@ -1,10 +1,27 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
 const expect = chai.expect;
+const server = require('../server');
 
-describe('/First test collection', function() {
-    it('should test two values', function() {
+chai.use(chaiHttp);
+
+describe('/First test collection', () => {
+    it('test default welcome API route', (done) => {
+        chai.request(server)
+        .get('/api/welcome')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            const actualVal = res.body.message;
+            expect(actualVal).to.be.equal("Welcome to the MEN RESTful API");
+            done();
+        })
+    })
+
+    it('should test two values', () => {
         //actual test content in here
         let expectedVal = 10;
         let actualVal = 10;
